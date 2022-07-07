1ea018a131cc96d7dec0879ccb773c52f23559f3
TreyMasta <93072511+TreyMasta@users.noreply.github.com>
2022-07-07 10:47:25 -0700

    Tests All Passed
    Tests all Passed

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -13,8 +13,51 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void correctLinks() throws IOException {
+    public void correctLinks1() throws IOException {
+        String content = Files.readString(Path.of("test-file.md"));
+        assertEquals(List.of("https://something.com", "some-thing.html"), MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void correctLinks2() throws IOException {
         String content = Files.readString(Path.of("test-file2.md"));
         assertEquals(List.of("https://google.com", "some-thing.html"), MarkdownParse.getLinks(content));
     }
+
+    @Test
+    public void correctLinks3() throws IOException {
+        String content = Files.readString(Path.of("test-file3.md"));
+        assertEquals(List.of("more text here"), MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void correctLinks4() throws IOException {
+        String content = Files.readString(Path.of("test-file4.md"));
+        assertEquals(List.of(), MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void correctLinks5() throws IOException {
+        String content = Files.readString(Path.of("test-file5.md"));
+        assertEquals(List.of("page.com"), MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void correctLinks6() throws IOException {
+        String content = Files.readString(Path.of("test-file6.md"));
+        assertEquals(List.of("page.com"), MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void correctLinks7() throws IOException {
+        String content = Files.readString(Path.of("test-file7.md"));
+        assertEquals(List.of(), MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void correctLinks8() throws IOException {
+        String content = Files.readString(Path.of("test-file8.md"));
+        assertEquals(List.of("a link on the first line"), MarkdownParse.getLinks(content));
+    }
+    
 }
