3b3519caee1545590402ffa3822ec527a8956371
TreyMasta <93072511+TreyMasta@users.noreply.github.com>
2022-07-07 10:31:06 -0700

    MarkDownParse updated
    Now accounts for infinite loops.

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,10 @@
-import static org.junit.Assert.*;
 import org.junit.*;
+import static org.junit.Assert.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class MarkdownParseTest {
 
     @Test
@@ -7,4 +12,9 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
 
+    @Test
+    public void correctLinks() throws IOException {
+        String content = Files.readString(Path.of("test-file2.md"));
+        assertEquals(List.of("https://google.com", "some-thing.html"), MarkdownParse.getLinks(content));
+    }
 }
--- test-file2.md
+++ test-file2.md
@@ -2,5 +2,5 @@
 
 [a link!](https://google.com)
 [another link!](some-thing.html)
-
+[](
 some paragraph text after the links
